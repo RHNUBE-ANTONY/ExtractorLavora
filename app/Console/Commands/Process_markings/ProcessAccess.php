cdd799e4775a20f5097064ee9dd20f973d298a2e
RHNUBE-ANTONY <afloresrhnube@gmail.com>
2026-04-11 12:13:27 -0500

    Eliminar código innecesario de verificación de estado en los comandos de ProcessAccess, ProcessComendia y ProcessTarela

--- app/Console/Commands/Process_markings/ProcessAccess.php
+++ app/Console/Commands/Process_markings/ProcessAccess.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands\Process_markings;
 
 use App\Jobs\ProcessAccessJobs;
-use App\Models\tareas_programadas;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
@@ -17,8 +16,6 @@ class ProcessAccess extends Command
      */
     public function handle()
     {
-        $tareas_programadas = tareas_programadas::where('type_process', 2)->get()->last();
-        if (!empty($tareas_programadas) && $tareas_programadas->estado == 0)  return 0;
         ProcessAccessJobs::dispatch();
         return 0;
     }
